09d032bc27cf1b7d9a1059f0816223f128a58007
Xiliris <trashyface22@gmail.com>
2024-08-06 00:08:13 +0200

    fix: merge conflict

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,127 @@
+import { useState, useEffect } from "react";
+import axiosInstance from "../modules/axiosInstance";
 import { CookiesProvider, useCookies } from "react-cookie";
+
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import Button from "./form/Button";
 import Logo from "../assets/logo.png";
-import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  const [cookies] = useCookies(["token"]);
+  const [cookies, setCookie] = useCookies(["token"]);
+  const [user, setUser] = useState<any>({});
+
+  const [scrollingUp, setScrollingUp] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [isWhyClicked, setIsWhyClicked] = useState(false);
+  const [isCommunityClicked, setIsCommunityClicked] = useState(false);
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      setScrollingUp(currentScrollY < lastScrollY);
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
+  const handleWhyClick = () => {
+    setIsWhyClicked(!isWhyClicked);
+  };
+
+  const handleCommunityClick = () => {
+    setIsCommunityClicked(!isCommunityClicked);
+  };
+
+  async function getProfile() {
+    try {
+      const res = await axiosInstance.post("/auth/me", {
+        token: cookies.token,
+      });
+
+      const data = await res.data;
+
+      if (data) {
+        setUser(data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
-    <CookiesProvider>
-      <nav className="flex flex-row justify-between items-center w-[80vw] py-5 fixed left-0 right-0 mx-auto z-50">
-        <Link to="/">
+    <nav
+      className={`fixed left-0 right-0 top-0 py-5 w-full bg-gray-800 transition-transform duration-300 ease-in-out ${
+        scrollingUp ? "translate-y-0" : "-translate-y-full"
+      } z-50`}
+    >
+      <div className="flex flex-row justify-between items-center w-[70vw] mx-auto">
+        <Link to="/" className="flex items-center space-x-2 cursor-pointer">
           <img src={Logo} alt="Logo" className="w-12 cursor-pointer" />
+          <span className="text-3xl font-bold text-primary cursor-pointer">
+            Clickpulse
+          </span>
         </Link>
 
-        {cookies.token ? (
-          <Link to="/dashboard">
-            <Button>Dashboard</Button>
+        <div className="flex space-x-8">
+          <div
+            onClick={handleWhyClick}
+            className="flex items-center space-x-1 hover:text-emphasis text-primary cursor-pointer text-xl"
+          >
+            <span>Why Clickpulse</span>
+            <FontAwesomeIcon
+              icon={faChevronDown}
+              className={`transform transition-transform ${
+                isWhyClicked ? "rotate-180" : "rotate-0"
+              }`}
+            />
+          </div>
+          <div
+            onClick={handleCommunityClick}
+            className="flex items-center space-x-1 hover:text-emphasis text-primary cursor-pointer text-xl"
+          >
+            <span>Community</span>
+            <FontAwesomeIcon
+              icon={faChevronDown}
+              className={`transform transition-transform ${
+                isCommunityClicked ? "rotate-180" : "rotate-0"
+              }`}
+            />
+          </div>
+          <Link
+            to="/pricing"
+            className="hover:text-emphasis text-primary cursor-pointer text-xl"
+          >
+            Pricing
           </Link>
-        ) : (
-          <Link to="/login">
-            <Button>Login</Button>
-          </Link>
-        )}
-      </nav>
-    </CookiesProvider>
+        </div>
+
+        <div className="flex items-center space-x-8">
+          {user ? (
+            <Link
+              to="/dashboard"
+              className="flex items-center space-x-1 hover:text-emphasis text-primary cursor-pointer text-xl"
+            >
+              {user.username}
+            </Link>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/start-free-trial">
+                <Button>Sign In</Button>
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
   );
 }
---
+++ src/pages/Maintenance.tsx
@@ -0,0 +1,28 @@
+import Logo from "../assets/logo.png";
+
+export default function Maintenance() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-default-200 text-center p-4">
+      <img src={Logo} alt="Logo" className="w-24 mb-8" />
+      <div className=" p-8 rounded-2xl max-w-lg w-full">
+        <h1 className="text-3xl font-bold text-primary mb-4">
+          We'll be back soon!
+        </h1>
+        <p className="text-secondary-100 mb-6">
+          Our website is currently under maintenance.<br></br>
+          Please be patient we will be back online ASAP.
+        </p>
+        <p className="text-secondary-100 mb-4">
+          If you need to contact us, please reach out at
+          <a href="mailto:support@example.com" className="text-emphasis">
+            {" "}
+            clickpulse@support.com{" "}
+          </a>
+        </p>
+      </div>
+      <footer className="mt-8 text-secondary-100">
+        &copy; {new Date().getFullYear()} Clickpulse. All rights reserved.
+      </footer>
+    </div>
+  );
+}
