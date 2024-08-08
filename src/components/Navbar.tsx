012d34c20989dbc3dcdf819d21c73e3713e307e6
Dimajkee <paul.pravdic8@gmail.com>
2024-08-09 01:03:03 +0200

    feature: responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,39 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, FC, ReactNode } from 'react';
 import axiosInstance from '../modules/axiosInstance';
-import { CookiesProvider, useCookies } from 'react-cookie';
-
+import { useCookies } from 'react-cookie';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import {
+  faChevronDown,
+  faBars,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
 import Button from './form/Button';
 import Logo from '../assets/logo.png';
 
-export default function Navbar() {
-  const [cookies, setCookie] = useCookies(['token']);
-  const [user, setUser] = useState<any>({});
+const DropdownItem: FC<{ to: string; children: ReactNode }> = ({
+  to,
+  children,
+}) => {
+  return (
+    <li className="flex gap-2 transform transition-transform duration-300 hover:scale-105">
+      <span className="text-primary text-sm mt-2 hover:text-emphasis">
+        <Link to={to}>{children}</Link>
+      </span>
+    </li>
+  );
+};
 
+export default function Navbar() {
+  const [cookies] = useCookies(['token']);
+  const [user, setUser] = useState<any>({});
   const [scrollingUp, setScrollingUp] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
   const [isWhyClicked, setIsWhyClicked] = useState(false);
   const [isCommunityClicked, setIsCommunityClicked] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     getProfile();
@@ -33,12 +51,35 @@ export default function Navbar() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, [lastScrollY]);
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsWhyClicked(false);
+        setIsCommunityClicked(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () =>
+      document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
   const handleWhyClick = () => {
     setIsWhyClicked(!isWhyClicked);
+    if (isCommunityClicked) setIsCommunityClicked(false);
   };
 
   const handleCommunityClick = () => {
     setIsCommunityClicked(!isCommunityClicked);
+    if (isWhyClicked) setIsWhyClicked(false);
+  };
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   async function getProfile() {
@@ -46,93 +87,219 @@ export default function Navbar() {
       const res = await axiosInstance.post('/auth/me', {
         token: cookies.token,
       });
-
-      const data = await res.data;
-
-      if (data) {
-        setUser(data);
-      }
+      setUser(res.data);
     } catch (err) {
       console.log(err);
     }
   }
 
   return (
-    <nav
-      className={`fixed left-0 right-0 top-0 py-5 w-full bg-gray-800 transition-transform duration-300 ease-in-out ${
-        scrollingUp ? 'translate-y-0' : '-translate-y-full'
-      } z-50`}
-    >
-      <div className="flex flex-row justify-between items-center w-[70vw] mx-auto">
-        <Link
-          to="/"
-          className="flex items-center space-x-2 cursor-pointer"
-        >
-          <img
-            src={Logo}
-            alt="Logo"
-            className="w-12 cursor-pointer"
-          />
-          <span className="text-3xl font-bold text-primary cursor-pointer">
-            Clickpulse
-          </span>
-        </Link>
-
-        <div className="flex space-x-8">
-          <div
-            onClick={handleWhyClick}
-            className="flex items-center space-x-1 hover:text-emphasis text-primary cursor-pointer text-xl"
-          >
-            <span>Why Clickpulse</span>
-            <FontAwesomeIcon
-              icon={faChevronDown}
-              className={`transform transition-transform ${
-                isWhyClicked ? 'rotate-180' : 'rotate-0'
-              }`}
-            />
-          </div>
-          <div
-            onClick={handleCommunityClick}
-            className="flex items-center space-x-1 hover:text-emphasis text-primary cursor-pointer text-xl"
-          >
-            <span>Community</span>
-            <FontAwesomeIcon
-              icon={faChevronDown}
-              className={`transform transition-transform ${
-                isCommunityClicked ? 'rotate-180' : 'rotate-0'
-              }`}
-            />
-          </div>
-          <Link
-            to="/pricing"
-            className="hover:text-emphasis text-primary cursor-pointer text-xl"
-          >
-            Pricing
+    <>
+      <nav
+        className={`fixed w-full py-4 bg-gray-800 transition-transform duration-300 ease-in-out ${
+          scrollingUp ? 'translate-y-0' : '-translate-y-full'
+        } z-50`}
+      >
+        <div className="relative flex justify-between items-center w-[70vw] max-w-7xl mx-auto px-4 lg:px-0 cursor-pointer lg-w-[90vw]">
+          <Link to="/" className="flex items-center space-x-2">
+            <img src={Logo} alt="Logo" className="w-12" />
+            <span className="text-2xl sm:text-3xl font-bold text-primary cursor-pointer">
+              Clickpulse
+            </span>
           </Link>
-        </div>
-        <div className="flex items-center space-x-8">
-          {user && user.username ? (
-            <Link
-              to="/dashboard"
-              className="flex items-center space-x-1 hover:text-emphasis text-primary cursor-pointer text-xl"
+
+          <div className="lg:hidden">
+            <button
+              onClick={toggleMobileMenu}
+              className="text-primary"
             >
-              {user.username}
-            </Link>
-          ) : (
-            <>
-              <Link
-                to="/login"
-                className="hover:text-emphasis text-primary cursor-pointer text-xl"
+              <FontAwesomeIcon
+                icon={isMobileMenuOpen ? faTimes : faBars}
+                size="lg"
+              />
+            </button>
+          </div>
+
+          <div className="hidden space-x-8 items-center lg:flex">
+            <div className="relative" ref={dropdownRef}>
+              <div
+                onClick={handleWhyClick}
+                className="flex items-center space-x-1 hover:text-emphasis text-primary text-lg md:text-xl cursor-pointer"
               >
-                Log In
+                <span>Why Clickpulse</span>
+                <FontAwesomeIcon
+                  icon={faChevronDown}
+                  className={`transition-transform ${
+                    isWhyClicked ? 'rotate-180' : 'rotate-0'
+                  }`}
+                />
+              </div>
+              {isWhyClicked && (
+                <div className="absolute top-full left-0 mt-2 w-48 bg-gray-700 text-primary rounded-lg shadow-lg">
+                  <ul className="p-4 space-y-2 text-sm">
+                    <DropdownItem to="/feature1">
+                      Feature 1
+                    </DropdownItem>
+                    <DropdownItem to="/feature2">
+                      Feature 2
+                    </DropdownItem>
+                    <DropdownItem to="/feature3">
+                      Feature 3
+                    </DropdownItem>
+                  </ul>
+                </div>
+              )}
+            </div>
+            <div className="relative" ref={dropdownRef}>
+              <div
+                onClick={handleCommunityClick}
+                className="flex items-center space-x-1 hover:text-emphasis text-primary text-lg md:text-xl cursor-pointer"
+              >
+                <span>Community</span>
+                <FontAwesomeIcon
+                  icon={faChevronDown}
+                  className={`transition-transform ${
+                    isCommunityClicked ? 'rotate-180' : 'rotate-0'
+                  }`}
+                />
+              </div>
+              {isCommunityClicked && (
+                <div className="absolute top-full left-0 mt-2 w-48 bg-gray-700 text-primary rounded-lg shadow-lg">
+                  <ul className="p-4 space-y-2 text-sm">
+                    <DropdownItem to="/community1">
+                      Community 1
+                    </DropdownItem>
+                    <DropdownItem to="/community2">
+                      Community 2
+                    </DropdownItem>
+                    <DropdownItem to="/community3">
+                      Community 3
+                    </DropdownItem>
+                  </ul>
+                </div>
+              )}
+            </div>
+            <Link
+              to="/pricing"
+              className="hover:text-emphasis text-primary text-lg md:text-xl"
+            >
+              Pricing
+            </Link>
+          </div>
+
+          <div className="hidden lg:flex items-center space-x-8">
+            {user && user.username ? (
+              <Link
+                to="/dashboard"
+                className="flex items-center space-x-1 hover:text-emphasis text-primary text-lg md:text-xl"
+              >
+                {user.username}
               </Link>
-              <Link to="/signup" className="cursor-pointer text-xl">
-                <Button>Sign In</Button>
-              </Link>
-            </>
-          )}
+            ) : (
+              <>
+                <Link
+                  to="/login"
+                  className="hover:text-emphasis text-primary text-lg md:text-xl"
+                >
+                  Log In
+                </Link>
+                <Link to="/signup">
+                  <Button>Sign In</Button>
+                </Link>
+              </>
+            )}
+          </div>
         </div>
-      </div>
-    </nav>
+
+        {isMobileMenuOpen && (
+          <div className="lg:hidden bg-gray-800 min-w-[90vw]">
+            <div className="flex flex-col items-center space-y-4 py-6">
+              <div className="w-full">
+                <div
+                  onClick={handleWhyClick}
+                  className="flex justify-between items-center w-full px-4 py-2 text-primary text-lg cursor-pointer hover:bg-gray-700"
+                >
+                  <span>Why Clickpulse</span>
+                  <FontAwesomeIcon
+                    icon={faChevronDown}
+                    className={`transition-transform ${
+                      isWhyClicked ? 'rotate-180' : 'rotate-0'
+                    }`}
+                  />
+                </div>
+                {isWhyClicked && (
+                  <ul className="bg-gray-700 text-primary">
+                    <DropdownItem to="/feature1">
+                      Feature 1
+                    </DropdownItem>
+                    <DropdownItem to="/feature2">
+                      Feature 2
+                    </DropdownItem>
+                    <DropdownItem to="/feature3">
+                      Feature 3
+                    </DropdownItem>
+                  </ul>
+                )}
+              </div>
+              <div className="w-full">
+                <div
+                  onClick={handleCommunityClick}
+                  className="flex justify-between items-center w-full px-4 py-2 text-primary text-lg cursor-pointer hover:bg-gray-700"
+                >
+                  <span>Community</span>
+                  <FontAwesomeIcon
+                    icon={faChevronDown}
+                    className={`transition-transform ${
+                      isCommunityClicked ? 'rotate-180' : 'rotate-0'
+                    }`}
+                  />
+                </div>
+                {isCommunityClicked && (
+                  <ul className="bg-gray-700 text-primary">
+                    <DropdownItem to="/community1">
+                      Community 1
+                    </DropdownItem>
+                    <DropdownItem to="/community2">
+                      Community 2
+                    </DropdownItem>
+                    <DropdownItem to="/community3">
+                      Community 3
+                    </DropdownItem>
+                  </ul>
+                )}
+              </div>
+              <Link
+                to="/pricing"
+                className="hover:text-emphasis text-primary text-lg"
+              >
+                Pricing
+              </Link>
+              <div className="flex flex-col items-center space-y-4">
+                {user && user.username ? (
+                  <Link
+                    to="/dashboard"
+                    className="flex items-center space-x-1 hover:text-emphasis text-primary text-lg"
+                  >
+                    {user.username}
+                  </Link>
+                ) : (
+                  <>
+                    <Link
+                      to="/login"
+                      className="hover:text-emphasis text-primary text-lg"
+                    >
+                      Log In
+                    </Link>
+                    <Link to="/signup">
+                      <Button>Sign In</Button>
+                    </Link>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
+      </nav>
+    </>
   );
 }
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,11 +1,11 @@
-import { FC, useState, useEffect } from "react";
-import { CookiesProvider, useCookies } from "react-cookie";
-import axiosInstance from "../../modules/axiosInstance";
+import { FC, useState, useEffect } from 'react';
+import { CookiesProvider, useCookies } from 'react-cookie';
+import axiosInstance from '../../modules/axiosInstance';
 
-import Navbar from "../../components/Navbar";
-import Header from "../../components/header";
-import Button from "../../components/form/Button";
-import { Link } from "react-router-dom";
+import Navbar from '../../components/Navbar';
+import Header from '../../components/header';
+import Button from '../../components/form/Button';
+import { Link } from 'react-router-dom';
 
 interface WebsiteData {
   domain: string;
@@ -14,7 +14,7 @@ interface WebsiteData {
 }
 
 const Dashboard: FC = () => {
-  const [cookies] = useCookies(["token"]);
+  const [cookies] = useCookies(['token']);
   const [websites, setWebsites] = useState<WebsiteData[]>([]);
 
   useEffect(() => {
@@ -23,7 +23,9 @@ const Dashboard: FC = () => {
 
   async function getWebsites() {
     try {
-      const response = await axiosInstance.get(`/dashboard/websites/${cookies.token}`);
+      const response = await axiosInstance.get(
+        `/dashboard/websites/${cookies.token}`
+      );
       console.log(response.data);
 
       setWebsites(response.data);
@@ -38,11 +40,18 @@ const Dashboard: FC = () => {
       <Header title="Dashboard" />
       <main className="mt-40 flex flex-col justify-center items-center w-[80%] m-auto">
         <h1 className="text-4xl text-emphasis">Select a website</h1>
-        <p className="text-primary text-center w-full">Choose a website to view analytics and insights.</p>
+        <p className="text-primary text-center w-full">
+          Choose a website to view analytics and insights.
+        </p>
         <div className="w-full mt-10 flex flex-col justify-center items-center">
           {websites && websites.length > 0 ? (
             websites.map((website, index) => (
-              <Website key={index} domain={website.domain} active={website.active} id={website.id} />
+              <Website
+                key={index}
+                domain={website.domain}
+                active={website.active}
+                id={website.id}
+              />
             ))
           ) : (
             <p>No websites found</p>
@@ -68,7 +77,9 @@ const Website: FC<WebsiteProps> = ({ domain, id }) => {
       <p className="text-emphasis text-2xl">{domain}</p>
 
       <Link to={`/dashboard/${id}`}>
-        <Button className="bg-emphasis text-background-100 px-6 py-2 rounded-md items-center font-medium">View</Button>
+        <Button className="bg-emphasis text-background-100 px-6 py-2 rounded-md items-center font-medium">
+          View
+        </Button>
       </Link>
     </div>
   );
