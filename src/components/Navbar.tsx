ab48914a87ddfb164a1dd2c7f9f2f0754cab8f5b
Dimajkee <paul.pravdic8@gmail.com>
2024-08-10 00:18:17 +0200

    feat: home page finalizing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,39 @@
-import { useState, useEffect, useRef, FC, ReactNode } from "react";
-import axiosInstance from "../modules/axiosInstance";
-import { useCookies } from "react-cookie";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
-import Button from "./form/Button";
-import Logo from "../assets/logo.png";
+import { useState, useEffect, useRef, FC, ReactNode } from 'react';
+import { motion } from 'framer-motion';
+import axiosInstance from '../modules/axiosInstance';
+import { useCookies } from 'react-cookie';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faChevronDown,
+  faBars,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+import Button from './form/Button';
+import Logo from '../assets/logo.png';
 
-const DropdownItem: FC<{ to: string; children: ReactNode }> = ({ to, children }) => {
+const DropdownItem: FC<{ to: string; children: ReactNode }> = ({
+  to,
+  children,
+}) => {
   return (
-    <li className="flex gap-2 transform transition-transform duration-300 hover:scale-105">
-      <span className="text-primary text-sm mt-2 hover:text-emphasis">
-        <Link to={to}>{children}</Link>
-      </span>
-    </li>
+    <Link to={to}>
+      <motion.span
+        whileHover={{ scale: 1.1 }}
+        style={{ cursor: 'pointer' }}
+      >
+        <li className="flex gap-2 transform transition-transform duration-300 hover:scale-105 justify-center pb-2">
+          <span className="text-primary text-sm mt-2 hover:text-emphasis cursor-pointer">
+            {children}
+          </span>
+        </li>
+      </motion.span>
+    </Link>
   );
 };
 
 export default function Navbar() {
-  const [cookies] = useCookies(["token"]);
+  const [cookies] = useCookies(['token']);
   const [user, setUser] = useState<any>({});
   const [scrollingUp, setScrollingUp] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
@@ -26,7 +41,8 @@ export default function Navbar() {
   const [isCommunityClicked, setIsCommunityClicked] = useState(false);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  const dropdownRef = useRef<HTMLDivElement>(null);
+  const whyDropdownRef = useRef<HTMLDivElement>(null);
+  const communityDropdownRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     getProfile();
@@ -39,22 +55,32 @@ export default function Navbar() {
       setLastScrollY(currentScrollY);
     };
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
 
-    return () => window.removeEventListener("scroll", handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, [lastScrollY]);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+      if (
+        whyDropdownRef.current &&
+        !whyDropdownRef.current.contains(event.target as Node)
+      ) {
         setIsWhyClicked(false);
+      }
+
+      if (
+        communityDropdownRef.current &&
+        !communityDropdownRef.current.contains(event.target as Node)
+      ) {
         setIsCommunityClicked(false);
       }
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener('mousedown', handleClickOutside);
 
-    return () => document.removeEventListener("mousedown", handleClickOutside);
+    return () =>
+      document.removeEventListener('mousedown', handleClickOutside);
   }, []);
 
   const handleWhyClick = () => {
@@ -73,7 +99,7 @@ export default function Navbar() {
 
   async function getProfile() {
     try {
-      const res = await axiosInstance.post("/auth/me", {
+      const res = await axiosInstance.post('/auth/me', {
         token: cookies.token,
       });
       setUser(res.data);
@@ -86,70 +112,101 @@ export default function Navbar() {
     <>
       <nav
         className={`fixed w-full py-4 bg-gray-800 transition-transform duration-300 ease-in-out ${
-          scrollingUp ? "translate-y-0" : "-translate-y-full"
+          scrollingUp ? 'translate-y-0' : '-translate-y-full'
         } z-50`}
       >
-        <div className="relative flex justify-between items-center w-[70vw] max-w-7xl mx-auto lg:px-0 cursor-pointer md:w-[90vw]">
+        <div className="relative flex justify-between items-center w-[70vw] md:w-[70vw] mx-auto lg:px-0">
           <Link to="/" className="flex items-center space-x-2">
             <img src={Logo} alt="Logo" className="w-12" />
-            <span className="text-2xl sm:text-3xl font-bold text-primary cursor-pointer">Clickpulse</span>
+            <span className="text-2xl sm:text-3xl font-bold text-primary cursor-pointer">
+              Clickpulse
+            </span>
           </Link>
 
-          <div className="hidden lg:flex">
-            <button onClick={toggleMobileMenu} className="text-primary">
-              <FontAwesomeIcon icon={isMobileMenuOpen ? faTimes : faBars} size="lg" />
+          <div className="hidden xl:flex">
+            <button
+              onClick={toggleMobileMenu}
+              className="text-primary"
+            >
+              <FontAwesomeIcon
+                icon={isMobileMenuOpen ? faTimes : faBars}
+                size="xl"
+              />
             </button>
           </div>
 
-          <div className="flex space-x-8 items-center lg:hidden">
-            <div className="relative" ref={dropdownRef}>
+          <div className="flex space-x-8 items-center xl:hidden">
+            <div className="relative" ref={whyDropdownRef}>
               <div
                 onClick={handleWhyClick}
                 className="flex items-center space-x-1 hover:text-emphasis text-primary text-lg md:text-xl cursor-pointer"
               >
-                <span>Why Clickpulse</span>
+                <span className=" cursor-pointer ">
+                  Why Clickpulse
+                </span>
                 <FontAwesomeIcon
                   icon={faChevronDown}
-                  className={`transition-transform ${isWhyClicked ? "rotate-180" : "rotate-0"}`}
+                  className={`transition-transform ${
+                    isWhyClicked
+                      ? 'rotate-180'
+                      : 'rotate-0 cursor-pointer '
+                  }`}
                 />
               </div>
               {isWhyClicked && (
                 <div className="absolute top-full left-0 mt-2 w-48 bg-gray-700 text-primary rounded-lg shadow-lg">
                   <ul className="p-4 space-y-2 text-sm">
-                    <DropdownItem to="/feature1">Feature 1</DropdownItem>
-                    <DropdownItem to="/feature2">Feature 2</DropdownItem>
-                    <DropdownItem to="/feature3">Feature 3</DropdownItem>
+                    <DropdownItem to="/feature1">
+                      Feature 1
+                    </DropdownItem>
+                    <DropdownItem to="/feature2">
+                      Feature 2
+                    </DropdownItem>
+                    <DropdownItem to="/feature3">
+                      Feature 3
+                    </DropdownItem>
                   </ul>
                 </div>
               )}
             </div>
-            <div className="relative" ref={dropdownRef}>
+            <div className="relative" ref={communityDropdownRef}>
               <div
                 onClick={handleCommunityClick}
                 className="flex items-center space-x-1 hover:text-emphasis text-primary md:text-lg text-xl cursor-pointer"
               >
-                <span>Community</span>
+                <span className=" cursor-pointer ">Community</span>
                 <FontAwesomeIcon
                   icon={faChevronDown}
-                  className={`transition-transform ${isCommunityClicked ? "rotate-180" : "rotate-0"}`}
+                  className={`transition-transform ${
+                    isCommunityClicked ? 'rotate-180' : 'rotate-0'
+                  }`}
                 />
               </div>
               {isCommunityClicked && (
                 <div className="absolute top-full left-0 mt-2 w-48 bg-gray-700 text-primary rounded-lg shadow-lg">
                   <ul className="p-4 space-y-2 text-sm">
-                    <DropdownItem to="/community1">Community 1</DropdownItem>
-                    <DropdownItem to="/community2">Community 2</DropdownItem>
-                    <DropdownItem to="/community3">Community 3</DropdownItem>
+                    <DropdownItem to="/community1">
+                      Community 1
+                    </DropdownItem>
+                    <DropdownItem to="/community2">
+                      Community 2
+                    </DropdownItem>
+                    <DropdownItem to="/community3">
+                      Community 3
+                    </DropdownItem>
                   </ul>
                 </div>
               )}
             </div>
-            <Link to="/pricing" className="hover:text-emphasis text-primary text-lg md:text-xl">
+            <Link
+              to="/pricing"
+              className="hover:text-emphasis text-primary text-lg md:text-xl cursor-pointer"
+            >
               Pricing
             </Link>
           </div>
 
-          <div className="lg:hidden flex items-center space-x-8">
+          <div className="xl:hidden flex items-center space-x-8">
             {user && user.username ? (
               <Link
                 to="/dashboard"
@@ -159,7 +216,10 @@ export default function Navbar() {
               </Link>
             ) : (
               <>
-                <Link to="/login" className="hover:text-emphasis text-primary md:text-lg text-xl">
+                <Link
+                  to="/login"
+                  className="hover:text-emphasis text-primary md:text-lg text-xl cursor-pointer"
+                >
                   Log In
                 </Link>
                 <Link to="/signup">
@@ -171,7 +231,7 @@ export default function Navbar() {
         </div>
 
         {isMobileMenuOpen && (
-          <div className="lg:hidden bg-gray-800 min-w-[90vw]">
+          <div className="hidden bg-gray-800 min-w-[90vw] xl:block">
             <div className="flex flex-col items-center space-y-4 py-6">
               <div className="w-full">
                 <div
@@ -181,14 +241,22 @@ export default function Navbar() {
                   <span>Why Clickpulse</span>
                   <FontAwesomeIcon
                     icon={faChevronDown}
-                    className={`transition-transform ${isWhyClicked ? "rotate-180" : "rotate-0"}`}
+                    className={`transition-transform ${
+                      isWhyClicked ? 'rotate-180' : 'rotate-0'
+                    }`}
                   />
                 </div>
                 {isWhyClicked && (
-                  <ul className="bg-gray-700 text-primary">
-                    <DropdownItem to="/feature1">Feature 1</DropdownItem>
-                    <DropdownItem to="/feature2">Feature 2</DropdownItem>
-                    <DropdownItem to="/feature3">Feature 3</DropdownItem>
+                  <ul className="text-primary">
+                    <DropdownItem to="/feature1">
+                      Feature 1
+                    </DropdownItem>
+                    <DropdownItem to="/feature2">
+                      Feature 2
+                    </DropdownItem>
+                    <DropdownItem to="/feature3">
+                      Feature 3
+                    </DropdownItem>
                   </ul>
                 )}
               </div>
@@ -200,18 +268,29 @@ export default function Navbar() {
                   <span>Community</span>
                   <FontAwesomeIcon
                     icon={faChevronDown}
-                    className={`transition-transform ${isCommunityClicked ? "rotate-180" : "rotate-0"}`}
+                    className={`transition-transform ${
+                      isCommunityClicked ? 'rotate-180' : 'rotate-0'
+                    }`}
                   />
                 </div>
                 {isCommunityClicked && (
                   <ul className="bg-gray-700 text-primary">
-                    <DropdownItem to="/community1">Community 1</DropdownItem>
-                    <DropdownItem to="/community2">Community 2</DropdownItem>
-                    <DropdownItem to="/community3">Community 3</DropdownItem>
+                    <DropdownItem to="/community1">
+                      Community 1
+                    </DropdownItem>
+                    <DropdownItem to="/community2">
+                      Community 2
+                    </DropdownItem>
+                    <DropdownItem to="/community3">
+                      Community 3
+                    </DropdownItem>
                   </ul>
                 )}
               </div>
-              <Link to="/pricing" className="hover:text-emphasis text-primary text-lg">
+              <Link
+                to="/pricing"
+                className="hover:text-emphasis text-primary text-lg"
+              >
                 Pricing
               </Link>
               <div className="flex flex-col items-center space-y-4">
@@ -224,7 +303,10 @@ export default function Navbar() {
                   </Link>
                 ) : (
                   <>
-                    <Link to="/login" className="hover:text-emphasis text-primary text-lg">
+                    <Link
+                      to="/login"
+                      className="hover:text-emphasis text-primary text-lg"
+                    >
                       Log In
                     </Link>
                     <Link to="/signup">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,31 @@
-import { Suspense, lazy } from "react";
-import { Link } from "react-router-dom";
-import Loading from "../components/Loading";
-import Globe from "../assets/globe.png";
+import { Suspense, lazy } from 'react';
+import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
+import Globe from '../assets/globe.png';
+import './globe.css';
 
-const Navbar = lazy(() => import("../components/Navbar"));
-const Button = lazy(() => import("../components/form/Button"));
-const Pricing = lazy(() => import("../components/Home/Pricing"));
-const Hero = lazy(() => import("../components/Home/Hero"));
-const Footer = lazy(() => import("../components/Home/Footer"));
+const Navbar = lazy(() => import('../components/Navbar'));
+const Button = lazy(() => import('../components/form/Button'));
+const Pricing = lazy(() => import('../components/Home/Pricing'));
+const Hero = lazy(() => import('../components/Home/Hero'));
+const Footer = lazy(() => import('../components/Home/Footer'));
 
 export default function Home() {
   return (
     <Suspense fallback={<Loading />}>
       <Navbar />
       <main className="relative min-w-screen min-h-screen">
-        <section className="flex flex-col md:w-[90vw] w-[70vw] m-auto justify-center items-start h-screen">
+        <section className="flex flex-col md:w-[70vw] w-[70vw] m-auto justify-center items-start h-screen">
           <div className="md:w-full w-[60%]">
             <h1 className="text-primary md:text-4xl text-6xl leading-tight">
-              Harness the <span className="text-emphasis">Pulse</span> of User Engagement
+              Harness the <span className="text-emphasis">Pulse</span>{' '}
+              of User Engagement
             </h1>
             <p className="text-secondary-100 mt-5 md:text-lg text-2xl">
-              Discover in-depth analytics and insights into every click, scroll, and interaction on your site.
-              ClickPulse empowers you to optimize your digital experience with real-time data tracking.
+              Discover in-depth analytics and insights into every
+              click, scroll, and interaction on your site. ClickPulse
+              empowers you to optimize your digital experience with
+              real-time data tracking.
             </p>
             <Link to="/login">
               <Button className="mt-5">Get Started</Button>
@@ -32,11 +36,11 @@ export default function Home() {
         <img
           src={Globe}
           alt="Globe"
-          className="opacity-80 xl:translate-y-56 lg:translate-y-44 md:translate-y-36 translate-y-28 xl:translate-x-80 lg:translate-x-60 md:translate-x-40 translate-x-20 2xl:w-[1050px] xl:w-[60vw] lg:w-[70vw] md:w-[60vw] sm:w-[80vw] w-[90vw] right-0 bottom-0 absolute max-w-none"
+          className="globe-img"
           style={{
-            pointerEvents: "none",
-            userSelect: "none",
-            touchAction: "none",
+            pointerEvents: 'none',
+            userSelect: 'none',
+            touchAction: 'none',
           }}
         />
       </main>
