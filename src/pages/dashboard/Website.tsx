42bb0e360a1beefbd0bfb864d26fe00e48f81b0f
Xiliris <trashyface22@gmail.com>
2024-09-18 01:31:30 +0200

    fix: Added dashboard range select

--- src/pages/dashboard/Website.tsx
+++ src/pages/dashboard/Website.tsx
@@ -1,119 +1,30 @@
-import { FC, Suspense, lazy } from 'react';
+import { FC, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
-const MultiValue = lazy(
-  () => import('../../components/dashboard/MultiValue')
-);
-const SingleValue = lazy(
-  () => import('../../components/dashboard/SingleValue')
-);
+import axiosInstance from '../../modules/axiosInstance';
 
 const Website: FC = () => {
   const { id } = useParams();
-  const today = new Date().toISOString().slice(0, 10);
+  const startDate = '2024-08-06';
+  const endDate = '2024-09-20';
+
+  useEffect(() => {
+    async function getTotalPageViews() {
+      const response = await axiosInstance.get(`/data/total-views/${id}?startDate=${startDate}&endDate=${endDate}`);
+      const data = await response.data;
+
+      console.log('Total Page Views:', data);
+    };
+
+
+    getTotalPageViews();
+  }, [startDate, endDate]);
 
   return (
     <main className="flex flex-col justify-center items-center">
       <h1 className="text-4xl text-emphasis">Website</h1>
 
       <Container title="Total">
-        <Suspense fallback={<div className="text-white">Loading...</div>}>
-          <SingleValue
-            id={id}
-            request={'total-views'}
-            date={today}
-            type={'views'}
-            title={'Total Views'}
-          />
-        </Suspense>
-
-        <Suspense fallback={<div className="text-white">Loading...</div>}>
-          <SingleValue
-            id={id}
-            request={'total-views'}
-            date={today}
-            type={'page_views'}
-            title={'Total Page Views'}
-          />
-        </Suspense>
-
-        <Suspense fallback={<div className="text-white">Loading...</div>}>
-          <SingleValue
-            id={id}
-            request={'bounce-rate'}
-            date={today}
-            type={'bounces'}
-            title={'Bounce Rate'}
-          />
-        </Suspense>
-
-        <Suspense fallback={<div className="text-white">Loading...</div>}>
-          <SingleValue
-            id={id}
-            request={'session-duration'}
-            date={today}
-            type={'duration'}
-            title={'Average Visit Duration'}
-          />
-        </Suspense>
-      </Container>
-
-      <Container title="Engagement Information">
-        <Suspense fallback={<div className="text-white">Loading...</div>}>
-          <MultiValue
-            id={id}
-            request={'entry-page'}
-            type={'path'}
-            title={'Entry Pages'}
-          />
-        </Suspense>
-
-        <Suspense fallback={<div className="text-white">Loading...</div>}>
-          <MultiValue
-            id={id}
-            request={'exit-page'}
-            type={'path'}
-            title={'Exit Pages'}
-          />
-        </Suspense>
-
-        <Suspense fallback={<div className="text-white">Loading...</div>}>
-          <MultiValue
-            id={id}
-            request={'visited-page'}
-            type={'path'}
-            title={'Visited Pages'}
-          />
-        </Suspense>
-      </Container>
-
-      <Container title="Client Information">
-        <Suspense fallback={<div className="text-white">Loading...</div>}>
-          <MultiValue
-            id={id}
-            request={'devices'}
-            type={'device'}
-            title={'Devices'}
-          />
-        </Suspense>
-
-        <Suspense fallback={<div className="text-white">Loading...</div>}>
-          <MultiValue
-            id={id}
-            request={'browsers'}
-            type={'browser'}
-            title={'Browsers'}
-          />
-        </Suspense>
-
-        <Suspense fallback={<div className="text-white">Loading...</div>}>
-          <MultiValue
-            id={id}
-            request={'os'}
-            type={'os'}
-            title={'Operating Systems'}
-          />
-        </Suspense>
+              <p className='text-white'>Testing</p>
       </Container>
     </main>
   );
