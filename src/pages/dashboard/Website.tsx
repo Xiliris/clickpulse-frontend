774ca5fbbaefa126415be0973a4e69a8de22d411
Adnan <trashyface22@gmail.com>
2024-09-22 03:59:43 +0200

    feat: Dashboard Graph

--- src/pages/dashboard/Website.tsx
+++ src/pages/dashboard/Website.tsx
@@ -1,30 +1,52 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import axiosInstance from '../../modules/axiosInstance';
 
 const Website: FC = () => {
-  const { id } = useParams();
+  const { id } = useParams<{ id: string }>();
+  const [totalVisits, setTotalVisits] = useState<{ date: string; views: number }[]>([]);
   const startDate = '2024-08-06';
   const endDate = '2024-09-20';
 
   useEffect(() => {
     async function getTotalPageViews() {
-      const response = await axiosInstance.get(`/data/total-views/${id}?startDate=${startDate}&endDate=${endDate}`);
-      const data = await response.data;
+      try {
+        const response = await axiosInstance.get(`/data/total-views/${id}?startDate=${startDate}&endDate=${endDate}`);
+        const data = response.data;
 
-      console.log('Total Page Views:', data);
-    };
-
+        const filterData = data.map(({ date, views }: { date: string; views: number }) => ({ date, views }));
+        setTotalVisits(filterData);
+      } catch (error) {
+        console.error("Error fetching total page views:", error);
+      }
+    }
 
     getTotalPageViews();
-  }, [startDate, endDate]);
+  }, [id, startDate, endDate]);
 
   return (
     <main className="flex flex-col justify-center items-center">
       <h1 className="text-4xl text-emphasis">Website</h1>
 
       <Container title="Total">
-              <p className='text-white'>Testing</p>
+        <ResponsiveContainer width="100%" height={200}>
+          <LineChart
+            data={totalVisits}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Line type="monotone" dataKey="views" stroke="#8884d8" fill="#8884d8" />
+          </LineChart>
+        </ResponsiveContainer>
       </Container>
     </main>
   );
