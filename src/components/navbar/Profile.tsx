e3066c88e219869c16e48462a79dd6aabd854bc4
Dimajkee <paul.pravdic8@gmail.com>
2024-10-18 23:03:59 +0200

    fix: fixed profile icon when logged in and not

--- src/components/navbar/Profile.tsx
+++ src/components/navbar/Profile.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faUser,
+  faBars,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
 
 interface ProfileMenuProps {
   user: { username: string } | null;
   isProfileMenuOpen: boolean;
-  isLogged: boolean;
   toggleProfileMenu: () => void;
-  toggleLoggedMenu: () => void;
   toggleMobileMenu: () => void;
   isMobileMenuOpen: boolean;
 }
@@ -16,63 +18,75 @@ interface ProfileMenuProps {
 const ProfileMenu: React.FC<ProfileMenuProps> = ({
   user,
   isProfileMenuOpen,
-  isLogged,
   toggleProfileMenu,
-  toggleLoggedMenu,
   toggleMobileMenu,
   isMobileMenuOpen,
 }) => {
-  const profileMenuRef = useRef<HTMLDivElement | null>(null);
+  const desktopProfileMenuRef = useRef<HTMLDivElement | null>(null);
+  const desktopProfileButtonRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent | TouchEvent) => {
       if (
-        profileMenuRef.current &&
-        !profileMenuRef.current.contains(event.target as Node)
+        desktopProfileMenuRef.current &&
+        !desktopProfileMenuRef.current.contains(
+          event.target as Node
+        ) &&
+        desktopProfileButtonRef.current &&
+        !desktopProfileButtonRef.current.contains(
+          event.target as Node
+        )
       ) {
         if (isProfileMenuOpen) {
           toggleProfileMenu();
         }
-        if (isLogged) {
-          toggleLoggedMenu();
-        }
       }
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
-    document.addEventListener("touchstart", handleClickOutside);
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('touchstart', handleClickOutside);
 
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-      document.removeEventListener("touchstart", handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('touchstart', handleClickOutside);
     };
-  }, [isProfileMenuOpen, isLogged, toggleProfileMenu, toggleLoggedMenu]);
+  }, [isProfileMenuOpen, toggleProfileMenu]);
 
   return (
     <>
       {user && user.username ? (
-        <div className="relative w-3/4 text-primary text-lg mx-auto hidden xl:flex justify-end mr-4">
+        <div
+          className="relative w-3/4 text-primary text-lg mx-auto hidden xl:flex justify-end mr-4"
+          ref={desktopProfileButtonRef}
+        >
           {/* User Icon */}
           <div className="flex justify-between items-center py-4">
-            <i
-              className="fa-solid fa-user text-secondary-100 mr-2 md:text-xl cursor-pointer"
+            <FontAwesomeIcon
+              icon={faUser}
+              className="text-secondary-100 mr-2 md:text-xl cursor-pointer"
               onClick={toggleProfileMenu}
-            ></i>
+            />
           </div>
 
           {/* Profile Dropdown */}
           {isProfileMenuOpen && (
-            <div className="absolute top-full right-0 w-56 bg-default-300 text-primary rounded-lg shadow-lg flex flex-col z-10">
+            <div
+              ref={desktopProfileMenuRef}
+              className="absolute top-full right-0 w-56 bg-default-300 text-primary rounded-lg shadow-lg flex flex-col z-10"
+            >
+              <span className="py-4 w-full font-bold text-xl text-center text-primary border-b-[1px] border-gray-600 hover:text-emphasis cursor-pointer">
+                {user.username}
+              </span>
               <Link
                 to="/dashboard"
-                className="py-4 w-full text-center text-primary border-b-[1px] border-gray-600 hover:text-emphasis cursor-pointer"
+                className="py-4 w-full text-center text-primary hover:text-emphasis cursor-pointer"
                 onClick={toggleProfileMenu}
               >
                 Dashboard
               </Link>
               <Link
                 to="/change-password"
-                className="py-4 w-full text-center text-primary border-b-[1px] border-gray-600 hover:text-emphasis cursor-pointer"
+                className="py-4 w-full text-center text-primary hover:text-emphasis cursor-pointer"
                 onClick={toggleProfileMenu}
               >
                 Change Password
@@ -87,38 +101,7 @@ const ProfileMenu: React.FC<ProfileMenuProps> = ({
             </div>
           )}
         </div>
-      ) : (
-        <div className="relative w-3/4 text-primary text-lg mx-auto hidden xl:flex justify-end mr-4">
-          {/* User Icon */}
-          <div className="flex justify-between items-center py-4 cursor-pointer">
-            <FontAwesomeIcon
-              icon={faUser}
-              className="text-secondary-100"
-              onClick={toggleLoggedMenu}
-            />
-          </div>
-
-          {/* Login/Signup Dropdown */}
-          {isLogged && (
-            <div className="absolute top-full right-0 w-40 bg-default-100 text-primary rounded-lg shadow-lg flex flex-col z-10">
-              <Link
-                to="/login"
-                className="py-2 mt-2 px-4 w-full text-start text-primary hover:text-emphasis"
-                onClick={toggleLoggedMenu}
-              >
-                Log in
-              </Link>
-              <Link
-                to="/signup"
-                className="py-2 mb-2 px-4 w-full text-start text-primary hover:text-emphasis"
-                onClick={toggleLoggedMenu}
-              >
-                Sign up
-              </Link>
-            </div>
-          )}
-        </div>
-      )}
+      ) : null}
 
       {/* Mobile Menu Toggle */}
       <div className="hidden xl:flex cursor-pointer hover:text-emphasis">
@@ -126,7 +109,7 @@ const ProfileMenu: React.FC<ProfileMenuProps> = ({
           <FontAwesomeIcon
             icon={isMobileMenuOpen ? faTimes : faBars}
             className={`cursor-pointer w-6 ${
-              isMobileMenuOpen ? "scale-[1.60]" : "scale-[1.37]"
+              isMobileMenuOpen ? 'scale-[1.60]' : 'scale-[1.37]'
             }`}
           />
         </button>
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,23 +1,23 @@
-import { useRef, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import Logo from "../../assets/logo.svg";
-import Background from "../../assets/background.svg";
+import { useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Logo from '../../assets/logo.svg';
+import Background from '../../assets/background.svg';
 
-import Input from "../../components/form/Input";
-import Button from "../../components/form/Button";
-import PasswordInput from "../../components/form/PasswordInput";
+import Input from '../../components/form/Input';
+import Button from '../../components/form/Button';
+import PasswordInput from '../../components/form/PasswordInput';
 
-import Header from "../../components/Header";
-import Spinner from "../../components/Spinner";
-import axiosInstance from "../../modules/axiosInstance";
+import Header from '../../components/Header';
+import Spinner from '../../components/Spinner';
+import axiosInstance from '../../modules/axiosInstance';
 
-import validatePassword from "../../utils/form/validatePassword";
-import validateUsername from "../../utils/form/validateUsername";
-import validateEmail from "../../utils/form/validateEmail";
+import validatePassword from '../../utils/form/validatePassword';
+import validateUsername from '../../utils/form/validateUsername';
+import validateEmail from '../../utils/form/validateEmail';
 
 export default function Register() {
   const navigate = useNavigate();
-  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
   const usernameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
@@ -32,12 +32,12 @@ export default function Register() {
     const confirmPassword = confirmPasswordRef.current?.value;
 
     if (!username || !email || !password || !confirmPassword) {
-      setErrorMessage("Please fill in all fields");
+      setErrorMessage('Please fill in all fields');
       return;
     }
 
     if (password !== confirmPassword) {
-      setErrorMessage("Passwords do not match");
+      setErrorMessage('Passwords do not match');
       return;
     }
 
@@ -54,14 +54,14 @@ export default function Register() {
     setLoading(true);
 
     try {
-      const response = await axiosInstance.post("/auth/register", {
+      const response = await axiosInstance.post('/auth/register', {
         username,
         email,
         password,
       });
 
       if (response.status === 200) {
-        navigate("/verify");
+        navigate('/verify');
       } else {
         setErrorMessage(response.data);
       }
@@ -76,16 +76,23 @@ export default function Register() {
     <>
       <Header title="Register" />
 
-      <main className="min-h-screen flex items-center justify-center bg-default-200 py-12 px-4 sm:px-6 lg:px-8 relative overflow-hidden">
+      <main className="h-full flex items-center justify-center bg-default-200 py-12 lg:py-0 px-4 sm:px-6 lg:px-8 relative overflow-hidden">
         <div
           className="absolute inset-0 bg-cover bg-center opacity-5 rotate-12 scale-[1.45]"
           style={{ backgroundImage: `url(${Background})` }}
         ></div>
         <div className="max-w-lg w-full space-y-8 bg-default-300 rounded-md p-10 z-20 relative overflow-hidden">
-          <Link to="/" className="text-emphasis cursor-pointer block text-xl">
+          <Link
+            to="/"
+            className="text-emphasis cursor-pointer block text-xl"
+          >
             <i className="fa-solid fa-arrow-left cursor-pointer"></i>
           </Link>
-          <img className="mx-auto h-16 w-auto" src={Logo} alt="Logo" />
+          <img
+            className="mx-auto h-16 w-auto"
+            src={Logo}
+            alt="Logo"
+          />
           <h2 className="mt-6 text-3xl font-extrabold text-primary text-center">
             Create a new account
           </h2>
@@ -104,34 +111,36 @@ export default function Register() {
               type="text"
               name="username"
               ref={usernameRef}
-              onChange={() => setErrorMessage("")}
+              onChange={() => setErrorMessage('')}
             />
             <Input
               placeholder="Email address"
               type="email"
               name="email"
               ref={emailRef}
-              onChange={() => setErrorMessage("")}
+              onChange={() => setErrorMessage('')}
             />
             <PasswordInput
               placeholder="Password"
               name="password"
               ref={passwordRef}
-              onChange={() => setErrorMessage("")}
+              onChange={() => setErrorMessage('')}
             />
             <PasswordInput
               placeholder="Confirm Password"
               name="confirm-password"
               ref={confirmPasswordRef}
-              onChange={() => setErrorMessage("")}
+              onChange={() => setErrorMessage('')}
             />
 
-            {errorMessage && <p className="text-red-400">{errorMessage}</p>}
+            {errorMessage && (
+              <p className="text-red-400">{errorMessage}</p>
+            )}
 
             <div className="justify-between flex pt-4">
               <button
                 type="button"
-                className="px-6 py-2 rounded-md inline-flex items-center text-center text-xl duration-200 ease-in-out prevent-select bg-default-200 border-2 border-secondary-200 cursor-pointer"
+                className="px-2 py-2 rounded-md inline-flex items-center text-center text-xl duration-200 ease-in-out prevent-select bg-default-200 border-2 border-secondary-200 cursor-pointer"
               >
                 <Link
                   to="/login"
