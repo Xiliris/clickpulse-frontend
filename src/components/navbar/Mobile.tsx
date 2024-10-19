b913073b0531460abfe614c7c006be247ac2575f
Adnan Skopljak <trashyface22@gmail.com>
2024-10-19 16:24:35 +0200

    Update Mobile.tsx

--- src/components/navbar/Mobile.tsx
+++ src/components/navbar/Mobile.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import Item from "./Item";
